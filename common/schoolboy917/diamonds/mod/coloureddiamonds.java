5edf55275419266d9a12afde695c9ecdf7775b16
gergobiacsko <biacskogergo@gmail.com>
2013-03-21 18:51:25 +0000

    Initial Commit of Mod Files

---
+++ common/schoolboy917/diamonds/mod/coloureddiamonds.java
@@ -0,0 +1,17 @@
+package schoolboy917.diamonds.mod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "ColouredDiamondsMod", name = "Coloured Diamonds Mod", version = "1.0")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class coloureddiamonds 
+{
+	@Init
+	public void load(FMLInitializationEvent event)
+	{
+		
+	}
+}
